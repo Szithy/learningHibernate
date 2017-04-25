e7e124d04b28202cc4c413fb255aaddd91421785
Szigethy Ádám <szithy@gmail.com>
2017-04-25 21:50:56 +0200

    mapping history set

--- src/hu/szithy/Program.java
+++ src/hu/szithy/Program.java
@@ -1,11 +1,11 @@
 package hu.szithy;
 
+import java.util.Date;
+
 import org.hibernate.Session;
 
 public class Program {
 
-	
-	
 	public static void main(String[] args) {
 		System.out.println("Hello world!");
 		Session session =  HibernateUtilities.getSessionFactory().openSession();
@@ -13,18 +13,25 @@ public class Program {
 		
 		User user = new User();
 		user.setName("Adam");
+		user.getHistory().add(new UserHistory(new Date(), "Set name to Adam"));
 		user.getProteinData().setGoal(250);
+		user.getHistory().add(new UserHistory(new Date(), "Set the goal to 250"));
 		session.save(user);
 		
 		session.getTransaction().commit();
 		
 		session.beginTransaction();
 		
-		User loadedUser = (User) session.load(User.class,1);
+		User loadedUser = (User) session.load(User.class,2);
 		System.out.println(loadedUser.getName());
 		System.out.println(loadedUser.getProteinData().getGoal());
 		
+		for(UserHistory history : loadedUser.getHistory()){
+			System.out.println(history.getEntryTime().toString() + " " + history.getEntry());
+		}
+		
 		loadedUser.getProteinData().setTotal(loadedUser.getProteinData().getTotal() + 50);
+		loadedUser.getHistory().add(new UserHistory(new Date(),"Added 50 protein"));
 		
 		session.getTransaction().commit();
 		
