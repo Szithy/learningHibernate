ac95a24c8aa033d0029b73bfc05b54ee5d6a1dab
Szigethy Ádám <szithy@gmail.com>
2017-04-26 19:37:06 +0200

    creating data to the database

--- src/hu/szithy/Program.java
+++ src/hu/szithy/Program.java
@@ -10,42 +10,42 @@ public class Program {
 
 	public static void main(String[] args) {
 		System.out.println("Hello world!");
+		
+		PopulateSimpleData();
+		
+		HibernateUtilities.getSessionFactory().close();
+	}
+
+	private static void PopulateSimpleData() {
 		Session session =  HibernateUtilities.getSessionFactory().openSession();
 		session.beginTransaction();
 		
-		User user = new User();
-		user.setName("Adam");
-		user.addHistory(new UserHistory(new Date(), "Set name to Adam"));
-		user.getProteinData().setGoal(250);
-		user.addHistory(new UserHistory(new Date(), "Set the goal to 250"));
+		User joe = CreateUser("Joe", 500, 50, "Good job", "You made it!");
+		session.save(joe);
 		
-		user.getGoalAlerts().add(new GoalAlert("Congrats!"));
-		user.getGoalAlerts().add(new GoalAlert("You did it!"));
+		User bob = CreateUser("Bob", 300, 20, "Taco time!");
+		session.save(bob);
 		
-		session.save(user);
+		User amy = CreateUser("Amy", 250, 200, "YESSS!");
+		session.save(amy);
 		
 		session.getTransaction().commit();
-		
-		session.beginTransaction();
-		
-		User loadedUser = (User) session.load(User.class,1);
-		System.out.println(loadedUser.getName());
-		System.out.println(loadedUser.getProteinData().getGoal());
-		
-		for(UserHistory history : loadedUser.getHistory()){
-			System.out.println(history.getEntryTime().toString() + " " + history.getEntry());
-		}
-		
-		loadedUser.getProteinData().setTotal(loadedUser.getProteinData().getTotal() + 50);
-		loadedUser.addHistory(new UserHistory(new Date(),"Added 50 protein"));
-		
-		user.setProteinData(new ProteinData());
-		
-		session.getTransaction().commit();
-		
 		session.close();
 		
-		HibernateUtilities.getSessionFactory().close();
+		
+	}
+
+	private static User CreateUser(String name, int goal, int total, String ... alerts) {
+		User user = new User();
+		user.setName(name);
+		user.getProteinData().setGoal(goal);
+		user.addHistory(new UserHistory(new Date(), "Set goal to: " + goal));
+		user.getProteinData().setTotal(total);
+		user.addHistory(new UserHistory(new Date(), "Set total to: " + total));
+		for (String alert : alerts){
+			user.getGoalAlerts().add(new GoalAlert(alert));
+		}
+		return user;
 	}
 
 }
